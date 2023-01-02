b317f68bf66216512a2b7c676e7467f4c33a77d3
Lucas Eduardo Campolino <campolino.l@gmail.com>
2023-01-02 15:39:59 -0300

    get geolocation

--- forecast.js
+++ forecast.js
@@ -1,6 +1,13 @@
-export default function forecast() {
+const searchButton = document.getElementById("search-button");
+
+searchButton.addEventListener("click", () => {
+  let cityName = document.getElementById("city-name").value;
+
+  forecast(cityName);
+});
+
+function forecast(cityName) {
   const API_KEY = "a698168853c8d391d9f68e0e0b8f5b58";
-  let cityName = "Joinville";
 
   fetch(
     `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${API_KEY}`
@@ -9,7 +16,9 @@ export default function forecast() {
       return res.json();
     })
     .then((data) => {
-      console.log(data.filter((city) => city.name == cityName));
+      const cities = data.filter((city) => city.name == cityName);
+      if (cities.length == 0) console.log("City not found!");
+      else cities.forEach((city) => console.log(`${city.lat} - ${city.lon}`));
     })
     .catch((err) => {
       console.log(err);
--- main.js
+++ main.js
@@ -1,3 +1,2 @@
-import forecast from "./forecast";
+import "./forecast";
 import "./style.css";
-forecast();
