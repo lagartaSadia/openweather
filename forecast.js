ccd2dc71686b463d5ca0823f2e5bb585b999e33c
Lucas Eduardo Campolino <campolino.l@gmail.com>
2023-01-03 23:54:03 -0300

    Create basics structure

--- forecast.js
+++ forecast.js
@@ -2,26 +2,26 @@ const API_KEY_FORECAST = "a698168853c8d391d9f68e0e0b8f5b58";
 const API_KEY_GIPHY = "X5BgOScTCzcvxmj68huYhrP7jwGurgb6";
 const searchButton = document.getElementById("search-button");
 
-searchButton.addEventListener("click", () => {
-  let userEntry = document.getElementById("city-name");
-  const data = userEntry.value.toUpperCase().split(", ");
+window.addEventListener("onload", getGif("cat city"));
 
-  forecast(data[0], data[1]);
-  userEntry.value = "";
+searchButton.addEventListener("click", () => {
+  const cityName = document.getElementById("search-box").value.toUpperCase();
+
+  forecast(cityName);
 });
 
 function forecast(cityName, country) {
   fetch(
-    `https://api.openweathermap.org/data/2.5/forecast?q=${cityName},${country}&units=metric&cnt=7&appid=${API_KEY_FORECAST}`
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${API_KEY_FORECAST}`
   )
     .then((res) => {
       return res.json();
     })
-    .then((weather) => {
-      if (weather.cod == "404") alert(weather.message);
+    .then((data) => {
+      if (data.cod == "404") alert(data.message);
       else {
-        console.log(weather);
-        getGif(weather);
+        console.log(data);
+        getGif(data["weather"][0]["main"]);
       }
     })
     .catch((err) => {
@@ -29,20 +29,23 @@ function forecast(cityName, country) {
     });
 }
 
-function getGif(weather) {
-  const weatherNow = weather["list"][0]["weather"][0]["main"];
-  const iconHeader = document.getElementById("icon-header");
+function getGif(weatherDescription) {
   fetch(
-    `https://api.giphy.com/v1/gifs/translate?s=${weatherNow}&api_key=${API_KEY_GIPHY}`
+    `https://api.giphy.com/v1/gifs/translate?s=${weatherDescription}&api_key=${API_KEY_GIPHY}`
   )
     .then((res) => {
       return res.json();
     })
     .then((gif) => {
-      console.log(gif);
-      iconHeader.src = gif["data"]["images"]["original"]["url"];
+      document.body.appendChild(populateImage(gif));
     })
     .catch((err) => {
       console.log(err);
     });
+}
+
+function populateImage(gif) {
+  const img = document.createElement("img");
+  img.src = gif["data"]["images"]["original"]["url"];
+  return img;
 }
